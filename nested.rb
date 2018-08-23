3428f0c7c8ebf4b2801400e6d983878d4a511bad
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-23 18:54:47 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,6 +1,7 @@
+require "pry"
 
 def hopper
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -16,13 +17,14 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
-	programmer_hash = 
+
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -37,10 +39,13 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -55,6 +60,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+programmer_hash[:dennis_ritchie][:languages] = "C"
+
 end
 
 def adding_matz
@@ -63,8 +71,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -80,11 +87,21 @@ def adding_matz
         }
      }
 
-    
+#this method adds to the top level of the hash  update = merge!
+programmer_hash.update :yukihiro_matsumoto => {
+	:known_for => "Ruby",
+	:languages => ["LISP", "C"]
+}
+
+# programmer_hash[:yukihiro_matsumoto] = {}
+# programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+# programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+# this Method only adds the key/value pair to the lower level of the hash
+#binding.pry
 end
 
 def changing_alan
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -99,14 +116,17 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-     
-     
+
+programmer_hash[:alan_kay][:known_for] = alans_new_info
+programmer_hash
+#binding.pry
+
 end
 
 def adding_to_dennis
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -121,5 +141,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+# binding.pry
+
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+programmer_hash
 
 end
